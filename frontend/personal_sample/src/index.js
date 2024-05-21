ea8a9a5ddf0f7d1520ba32e036a64d8c4a67a86f
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-21 23:33:10 +0900

    始まりを/Homeに変更（ボタンで画面の遷移）

--- frontend/personal_sample/src/index.js
+++ frontend/personal_sample/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<App />} />
+          </Routes>
+      </Router>
   </React.StrictMode>
 );
 
