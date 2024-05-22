877c33e32940fcea6d89888a11e7031ba1e379f7
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-22 17:29:30 +0900

    追加の処理をApp.jsから移動

--- frontend/personal_sample/src/components/UserForm.js
+++ frontend/personal_sample/src/components/UserForm.js
@@ -1,31 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './UserForm.css';
+import axios from 'axios';
 
-function UserForm({ addUser, updateUser, editing, currentUser, clearForm }) {
+function UserForm() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        if (editing) {
-            setName(currentUser.name);
-            setEmail(currentUser.email);
-            setPassword(currentUser.password);
-        } else {
-            clearForm();
-        }
-    }, [editing, currentUser]);
+    // useEffect(() => {
+    //     if (editing) {
+    //         setName(currentUser.name);
+    //         setEmail(currentUser.email);
+    //         setPassword(currentUser.password);
+    //     } else {
+    //         console.log(editing);
+    //         clearForm();
+    //     }
+    // }, [editing, currentUser]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (editing) {
-            updateUser({ name, email, password });
-        } else {
-            addUser({ name, email, password });
-        }
+        addUser({ name, email, password });
+        navigate('/');
+
+        setName('');
+        setEmail('');
+        setPassword('');
+    };
+
+    //追加の処理
+    const addUser = (newUser) => {
+        axios.post('http://localhost:8080/api/users', newUser)
+            .then(response => setUsers([...users, response.data]))
+            .catch(error => console.error(error));
     };
 
     return (
-        <div>
+        <div className='user-form'>
+            <h1>登録</h1>
             <input
                 type="text"
                 placeholder="名前"
@@ -39,13 +54,13 @@ function UserForm({ addUser, updateUser, editing, currentUser, clearForm }) {
                 onChange={(e) => setEmail(e.target.value)}
             />
             <input
-                type="text"
+                type="password"
                 placeholder="パスワード"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
             <button onClick={handleSubmit}>
-                {editing ? '更新' : '追加'}
+                追加
             </button>
         </div>
     );
