61ad349d975ed2b79cb0593d9154f2c7c00d0483
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-23 07:27:13 +0900

    emailとpasswordが一致するデータを抽出

--- backend/personal_sample_back/src/main/java/com/example/demo/UserService.java
+++ backend/personal_sample_back/src/main/java/com/example/demo/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,5 +41,10 @@ public class UserService {
 	public void deleteUser(Long id) {
 		userRepository.deleteById(id);
 	}
+	
+	public Optional<User> getUserByEmailAndPassword(String email, String password) {
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+	
 	
 }
