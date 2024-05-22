049e10e52c36ab921b1478735b5e2b24210a1644
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-23 07:26:42 +0900

    emailとpasswordが一致するデータを抽出

--- backend/personal_sample_back/src/main/java/com/example/demo/UserRepository.java
+++ backend/personal_sample_back/src/main/java/com/example/demo/UserRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-
+	
+	@Query("SELECT u FROM User u WHERE u.email = ?1 AND u.password = ?2")
+	Optional<User> findByEmailAndPassword(String email, String password);
+	
 }
 
