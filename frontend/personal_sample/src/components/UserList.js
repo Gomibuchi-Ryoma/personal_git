56dc1b30966201a15e810daa991edb8b7237206c
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-22 17:21:01 +0900

    特に変更なし

--- frontend/personal_sample/src/components/UserList.js
+++ frontend/personal_sample/src/components/UserList.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import './UserList.css';
+import { useNavigate } from 'react-router-dom';
+import {useState} from 'react';
 
 function UserList({ users, editUser, deleteUser }) {
     return (
-        <ul>
+        <>
+        <table>
+            <thead>
+                <tr>
+                    <th>名前</th>
+                    <th>メールアドレス</th>
+                    <th>パスワード</th>
+                    <th>編集</th>
+                    <th>削除</th>
+                </tr>
+            </thead>
+            <tbody>
             {users.map(user => (
-                <li key={user.id}>
-                    {user.name} - {user.email} - {user.password}
-                    <div>
-                        <button onClick={() => editUser(user)}>更新</button>
-                        <button className="delete" onClick={() => deleteUser(user.id)}>削除</button>
-                    </div>
-                </li>
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.password}</td>
+                    <td className='bt'><button className="edit" onClick={() => editUser(user)}>更新</button></td>
+                    <td className='bt'><button className="delete" onClick={() => deleteUser(user.id)}>削除</button></td>
+                </tr>
             ))}
-        </ul>
+            </tbody>
+        </table>
+        </>
     );
 }
 
