a13c978a8e8b6b0ce8b113b295eef3daf3eee84f
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-22 23:01:37 +0900

    入力に間違いがある場合追加できない

--- frontend/personal_sample/src/components/UserForm.js
+++ frontend/personal_sample/src/components/UserForm.js
@@ -11,15 +11,28 @@ function UserForm() {
     const [users, setUsers] = useState([]);
     //パスワードの表示保持
     const [showPassword, setShowPassword] = useState(false);
+    const [isFormValid, setIsFormValid] = useState(false); // フォームの有効性の状態
+    const [errorMessage, setErrorMessage] = useState('');//エラーメッセージ
+
+    // フォームの有効性をチェックする
+    useEffect(() => {
+        const isEmailValid = email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+        const isPasswordValid = password.length >= 8 && /[A-Z]/.test(password) && /[a-z]/.test(password) && /\d/.test(password);
+        setIsFormValid(name.trim() !== '' && isEmailValid && isPasswordValid);
+        setErrorMessage(''); // 入力が変更されたらエラーメッセージをクリアする
+    }, [name, email, password]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        addUser({ name, email, password });
-        navigate('/');
-
-        setName('');
-        setEmail('');
-        setPassword('');
+        if (isFormValid) {
+            addUser({ name, email, password });
+            navigate('/');
+            setName('');
+            setEmail('');
+            setPassword('');
+        } else {
+            setErrorMessage('正しく入力してください');
+        }
     };
 
     //追加の処理
@@ -52,7 +65,7 @@ function UserForm() {
             <div className="password-input-container">
                 <input
                     type={showPassword ? "text" : "password"} // パスワードの表示状態によってタイプを切り替える
-                    placeholder="パスワード"
+                    placeholder="パスワード(大小英数字8文字以上)"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
@@ -60,9 +73,11 @@ function UserForm() {
                     {showPassword ? "非表示" : "表示"}
                 </button>
             </div>
-            <button onClick={handleSubmit}>
+            
+            <button className='submit-button' onClick={handleSubmit}>
                 追加
             </button>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
         </div>
     );
 }
