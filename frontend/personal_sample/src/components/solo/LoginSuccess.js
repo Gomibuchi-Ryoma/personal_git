430ef5bd5ae6bd07ead3841aa84f837689541985
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-23 18:09:19 +0900

    ログイン成功時の画面作成

--- frontend/personal_sample/src/components/solo/LoginSuccess.js
+++ frontend/personal_sample/src/components/solo/LoginSuccess.js
@@ -1,21 +1,41 @@
 import React, { useEffect,useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation,useNavigate } from "react-router-dom";
+import './LoginSuccess.css'
 
 function LoginSuccsess(){
 
     const [name, setName] = useState('');
+    const [userId, setUserId] = useState(0);
     const location = useLocation();
     const user = location.state ? location.state.user : null;
+    const navigate = useNavigate();
 
     useEffect(() => {
-        if(user){
-            setName(user.name || ''); 
+        if (user) {
+            setName(user.name || '');
+            setUserId(user.id || 0);
             console.log(user);
         }
-    },[user])
+    }, [user]);
+
+
+    const createCards = () => { 
+        console.log('Navigating with userId:', userId);
+        navigate('/createCard', { state: { userId } });
+    };
+    const historyCards = () => { 
+        console.log('Navigating with userId:', userId);
+        navigate('/historyCard', { state: { userId } });
+    };
 
     return(
-        <h1>{name}さんこんにちは！</h1>
+        <div className="container">
+            <h1>{name}さんこんにちは！</h1>
+            <button onClick={createCards}>自己紹介カードを追加する</button>
+            <button onClick={historyCards}>過去の自己紹介カードを見る</button>
+        </div>
+        
+        
     );
 }
 
