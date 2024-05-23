6f360dbe5041c7c22cbfbcdf4a74247a6c5e82a7
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-23 18:03:28 +0900

    @Repositoryの削除

--- backend/personal_sample_back/src/main/java/com/example/demo/CardRepository.java
+++ backend/personal_sample_back/src/main/java/com/example/demo/CardRepository.java
@@ -1,9 +1,8 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface CardRepository extends JpaRepository<Card,Long> {
 
 }
