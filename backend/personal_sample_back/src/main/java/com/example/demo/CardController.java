735fe59be66e1655c5275264fd5893a2766818b0
Gomibuchi <ryoma.gomibuchi@g.dreamcareer.co.jp>
2024-05-23 18:03:09 +0900

    @CrossOriginの変更

--- backend/personal_sample_back/src/main/java/com/example/demo/CardController.java
+++ backend/personal_sample_back/src/main/java/com/example/demo/CardController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/cards")
-@CrossOrigin(origins = "https://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CardController {
 	
 	@Autowired
